72851c31b683c04c92d3b811a2f22a6ec3319e88
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-11-24 10:57:28 +0530

    created sample signup api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const port = 3000;
+
+// user signup api
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User({
+      firstName: "Pooja",
+      lastName: "Patil",
+      age: 28,
+      gender: "Female",
+      emailId: "patilpooja@gmail.com",
+    });
+    await user.save();
+    console.log(`User signup is successful!`);
+    return res.status(200).send("User signup is successful!");
+  } catch (err) {
+    console.log(`Err @ user signup : ${JSON.stringify(err)}`);
+    return res
+      .status(500)
+      .send(err.message || "Something went wrong at user signup!");
+  }
+});
 
 // first connect to db then start server application
 connectDB()
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://premuhulikoppe_db_user:4Pu2WRnWaB5t2muC@cluster0.jpvvdan.mongodb.net/?appName=Cluster0/dev_tinder"
+    "mongodb+srv://premuhulikoppe_db_user:4Pu2WRnWaB5t2muC@cluster0.jpvvdan.mongodb.net/dev_tinder_db"
   );
 };
 
