c4862f2975be3406e2a5b8cd2b5f7dde453d2a67
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-11-26 18:25:36 +0530

    added user auth using jwt & cookies

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const { validateSignupData, validateLoginData } = require("./middlewares/user");
+const { userAuth } = require("./middlewares/auth");
 const port = 3000;
 const privateKey = "DevTinder@1551";
 
@@ -61,10 +62,15 @@ app.post("/login", validateLoginData, async (req, res) => {
     }
 
     // generate auth token and set on res cookies for authentication
-    const authToken = await jwt.sign({ id: user?._id }, privateKey);
+    const authToken = await jwt.sign({ id: user?._id }, privateKey, {
+      expiresIn: "1d",
+    });
     console.log("User logged in is successfully!");
     // set auth token on cookies
-    res.cookie("token", authToken);
+    res.cookie("token", authToken, {
+      // cookie will be removed after 1day
+      expires: new Date(Date.now() + 5000),
+    });
     return res.status(200).send("User login is successful!");
   } catch (err) {
     console.log(`Err @ user signup : ${JSON.stringify(err)}`);
@@ -74,147 +80,28 @@ app.post("/login", validateLoginData, async (req, res) => {
   }
 });
 
-// GET user by emailId
-app.get("/user", async (req, res) => {
+// GET user /profile
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const user = await User.findOne({ emailId: req.body.emailId });
-    if (!user) {
-      return res.status(404).send("User not found!");
-    } else {
-      return res.send(user);
-    }
-  } catch (err) {
-    console.log(`Err @ get user by emailId : ${JSON.stringify(err)}`);
-    return res
-      .status(500)
-      .send(`ERROR : ${err.message || "Something went wrong!"}`);
-  }
-});
-
-// GET feed API - get all users from db
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      return res.status(404).send("No users exist!");
-    } else {
-      return res.send(users);
-    }
-  } catch (err) {
-    console.log(`Err @ feed users : ${JSON.stringify(err)}`);
-    return res
-      .status(500)
-      .send(`ERROR : ${err.message || "Something went wrong!"}`);
-  }
-});
-
-// GET user by Id
-app.get("/userById/:id", async (req, res) => {
-  try {
-    // validate the Id
-    if (!isObjectIdOrHexString(req.params.id)) {
-      return res.status(400).send("ERROR : Invalid userId!");
-    } else {
-      const user = await User.findById(req.params.id);
-      if (!user) {
-        return res.status(404).send("User not found!");
-      } else {
-        return res.send(user);
-      }
-    }
-  } catch (err) {
-    console.log(`Err @ get userById : ${JSON.stringify(err)}`);
-    return res
-      .status(500)
-      .send(`ERROR : ${err.message || "Something went wrong!"}`);
-  }
-});
-
-// DELETE user by Id
-app.delete("/user", async (req, res) => {
-  try {
-    // validate the Id
-    if (!req.body.userId) {
-      return res.status(400).send("Please provide userId!");
-    } else if (!isObjectIdOrHexString(req.body.userId)) {
-      return res.status(400).send("ERROR : Invalid userId!");
-    } else {
-      await User.findByIdAndDelete(req.body.userId);
-      return res.send("Deleted user successfully!");
-    }
-  } catch (err) {
-    console.log(`Err @ delete user by Id : ${JSON.stringify(err)}`);
-    return res
-      .status(500)
-      .send(`ERROR : ${err.message || "Something went wrong!"}`);
-  }
-});
-
-// UPDATE user by Id
-app.patch("/user/:userId", async (req, res) => {
-  try {
-    // validate the Id
-    if (!isObjectIdOrHexString(req.params?.userId)) {
-      return res.status(400).send("ERROR : Invalid userId!");
-    }
-    // validate allowed update keys eg : update should be denied for emailId
-    const ALLOWED_UPDATE_KEYS = [
-      "password",
-      "age",
-      "profileUrl",
-      "about",
-      "skills",
-    ];
-    // check for allowed update
-    const notAllowedKeys = Object.keys(req.body)?.filter(
-      (x) => !ALLOWED_UPDATE_KEYS.includes(x)
-    );
-    if (!!notAllowedKeys && !!notAllowedKeys?.length) {
-      return res
-        .status(400)
-        .send(
-          `Update is not allowed for keys ${JSON.stringify(notAllowedKeys)}!`
-        );
-    }
-    // update and return latest updated user data
-    const user = await User.findByIdAndUpdate(req.params?.userId, req.body, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).send("User not found!");
-    } else {
-      return res.send({ msg: "Updated user successfully", data: user });
-    }
-  } catch (err) {
-    console.log(`Err @ update user by Id : ${JSON.stringify(err)}`);
-    return res
-      .status(500)
-      .send(`ERROR : ${err.message || "Something went wrong!"}`);
-  }
-});
-
-// GET /profile
-app.get("/profile", async (req, res) => {
-  try {
-    // first check authentication
-    const { token } = req.cookies;
-    let decoded;
-    try {
-      decoded = await jwt.verify(token, privateKey);
-    } catch (error) {
-      return res
-        .status(400)
-        .send(`Auth ERROR : ${error?.message}, please login again!`);
-    }
-    // get user info and send in response back
-    const user = await User.findById(decoded?.id);
-    if (!user) {
-      return res.status(404).send("User not exists!");
-    }
+    // access userInfo from request object and respond same
+    const user = req?.userInfo;
     return res.status(200).send(user);
   } catch (err) {
     console.log(`Err @ get /profile : ${JSON.stringify(err)}`);
+    return res
+      .status(500)
+      .send(`ERROR : ${err.message || "Something went wrong!"}`);
+  }
+});
+
+// POST /sendConnectionRequest
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .send(req?.userInfo?.firstName + " send connection request!");
+  } catch (err) {
+    console.log(`Err @  /sendConnectionRequest : ${JSON.stringify(err)}`);
     return res
       .status(500)
       .send(`ERROR : ${err.message || "Something went wrong!"}`);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,26 +1,33 @@
-const adminAuth = (req, res, next) => {
-  console.log(`Entered admin auth middleware!`);
-  const token = "xyz";
-  const adminAuthorized = token === "xyz";
-  if (!adminAuthorized) {
-    return res.status(401).send("Admin unauthorized!");
-  } else {
-    next();
-  }
-};
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const privateKey = "DevTinder@1551";
 
-const userAuth = (req, res, next) => {
-  console.log(`Entered user auth middleware!`);
-  const token = "xyz";
-  const userAuthorized = token === "xyz";
-  if (!userAuthorized) {
-    return res.status(401).send("User unauthorized!");
-  } else {
+const userAuth = async (req, res, next) => {
+  try {
+    // extract auth token from req cookies and verify it
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    const decoded = await jwt.verify(token, privateKey);
+    // check user
+    const user = await User.findById(decoded?.id);
+    if (!user) {
+      throw new Error("User not exists");
+    }
+    // set user data on request body
+    req.userInfo = user;
     next();
+  } catch (err) {
+    console.log(`Err @ userAuth : ${JSON.stringify(err)}`);
+    return res
+      .status(400)
+      .send(
+        `ERROR : ${err.message || "Something went wrong"}, Please login again!`
+      );
   }
 };
 
 module.exports = {
-  adminAuth,
   userAuth,
 };
