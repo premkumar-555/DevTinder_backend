a6797179bd9372edf88421ba82e3105e2ea343f5
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-11-29 15:43:42 +0530

    created compund index on fileds fromUserId & toUserId connection_requests collection

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -29,6 +29,16 @@ const connectionRequestSchema = new mongoose.Schema(
 );
 
 /**
+ * creating a compoung index on fields fromUserId & toUserId
+ * creating indexes is good for development but not feasible for production
+ * as if db under heavy write ops index builds on updates will impact db performance
+ * which leads write ops very slow due to respective index builds so we should not alow this in prod
+ * we need to do index builds during write peak-off maintainance downtime period like night or can follow other methods
+ */
+
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+/**
  * Check fromUserId ===  toUserId (Not allow user to create request to himself)
  * a) If fromUserId & toUserId are equal then deny creation of request
  * b) else allow for creation
