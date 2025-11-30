0b3f55e6cc99af43212fca678c9b4d0cbb355603
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-11-30 21:47:30 +0530

    created /user/connections api & tested same

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,15 @@ const User = require("../models/user");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 
+const SELECTIVE_REF_FIELDS = [
+  "firstName",
+  "lastName",
+  "age",
+  "gender",
+  "about",
+  "skills",
+];
+
 // GET /user/review/requests/received : fetch connection requests received
 userRouter.get("/requests/received", userAuth, async (req, res) => {
   try {
@@ -17,17 +26,50 @@ userRouter.get("/requests/received", userAuth, async (req, res) => {
         status: "interested",
       },
       { toUserId: 0, status: 0, __v: 0 }
-    ).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "age",
-      "gender",
-      "about",
-      "skills",
-    ]);
+    ).populate("fromUserId", SELECTIVE_REF_FIELDS);
     res.status(200).send({ data: conReqs });
   } catch (err) {
     console.log(`Err @  /user/requests/received : ${JSON.stringify(err)}`);
+    return res
+      .status(500)
+      .send(`ERROR : ${err?.message || "Something went wrong!"}`);
+  }
+});
+
+// GET /user/connections : fetch all user connections
+userRouter.get("/connections", userAuth, async (req, res) => {
+  try {
+    // Edge cases
+    // 1. loggedInUser may be either sender or receiver of the request
+    // 2. connection request should be in 'accepted' status
+    // 3. Response data must be users list other than loggedInUser
+    const loggedInUser = req.userInfo;
+    let conReqs = await ConnectionRequest.find(
+      {
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+        status: "accepted",
+      },
+      { fromUserId: 1, toUserId: 1 }
+    )
+      .populate({
+        path: "fromUserId",
+        match: { _id: { $ne: loggedInUser._id } }, // Join condition on ref document
+        as: "user",
+        select: SELECTIVE_REF_FIELDS,
+      })
+      .populate({
+        path: "toUserId",
+        match: { _id: { $ne: loggedInUser._id } }, // Join condition on ref document
+        select: SELECTIVE_REF_FIELDS,
+      });
+    if (conReqs?.length > 0) {
+      conReqs = conReqs.map(({ fromUserId, toUserId }) =>
+        fromUserId && fromUserId?._id ? fromUserId : toUserId
+      );
+    }
+    return res.status(200).send({ data: conReqs });
+  } catch (err) {
+    console.log(`Err @  /user/connections : ${JSON.stringify(err)}`);
     return res
       .status(500)
       .send(`ERROR : ${err?.message || "Something went wrong!"}`);
